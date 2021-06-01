ba6ce66d520720978747b60c60de552a080231b5
Alex34972 <alex_hifi@ukr.net>
2021-06-02 00:10:11 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,5 @@
 import cantriesCardTempl from "../templates/cantries-card.hbs";
-
+import cantriesSearch from "../templates/search-cantries.hbs"
 let debounce = require('lodash.debounce');
 
 function fetchCountries(searchCountries) {
@@ -11,16 +11,28 @@ function fetchCountries(searchCountries) {
   }
 const refs = {
     input: document.querySelector(".search"),
-    cardContainer:document.querySelector(".countries")
+    cardContainer:document.querySelector(".countries-card"),
+    cantrie:document.querySelector(".countries-search")
   };
   refs.input.addEventListener(`input`,debounce(onSearch,500));
 
   function onSearch(e) {
     e.preventDefault();
     const searchCountries = e.target.value;
-    fetchCountries(searchCountries).then(cantries => {
-      const marcup = cantriesCardTempl(...cantries);
-      refs.cardContainer.innerHTML = marcup} 
-    )};
+    fetchCountries(searchCountries).then(renderCard)};
   
-//function renderCard(cantries)
+function renderCard(cantries){
+  console.log(cantries)
+  const marcup = cantriesCardTempl(...cantries);
+ if (cantries.length===1) {
+  refs.cardContainer.innerHTML = marcup;
+  refs.cantrie.innerHTML = ``;
+  return
+ }console.log(cantries);
+ const marcups = cantriesSearch(cantries);
+ console.log(marcups);
+ refs.cantrie.innerHTML= marcups;
+
+ 
+
+  } 
