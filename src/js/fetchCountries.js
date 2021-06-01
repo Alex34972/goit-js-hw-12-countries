82f7d8ce6210c64e1743289228f2fc50c4c6c872
Alex34972 <alex_hifi@ukr.net>
2021-06-01 22:07:20 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,26 @@
-function fetchCountries (countrieName){
-    return fetch(`https://restcountries.eu/rest/v2/name/${countrieName}`).then(response=>{
-        return response.json()
-    })
-}
-const inputEl = document.querySelector('.search');
+import cantriesCardTempl from "../templates/cantries-card.hbs";
+
+let debounce = require('lodash.debounce');
+
+function fetchCountries(searchCountries) {
+    return fetch(`https://restcountries.eu/rest/v2/name/${searchCountries}`).then(
+      (res) => {
+        return res.json();
+      }
+    );
+  }
+const refs = {
+    input: document.querySelector(".search"),
+    cardContainer:document.querySelector(".countries")
+  };
+  refs.input.addEventListener(`input`,debounce(onSearch,500));
+
+  function onSearch(e) {
+    e.preventDefault();
+    const searchCountries = e.target.value;
+    fetchCountries(searchCountries).then(cantries => {
+      const marcup = cantriesCardTempl(...cantries);
+      refs.cardContainer.innerHTML = marcup} 
+    )};
+  
+//function renderCard(cantries)
