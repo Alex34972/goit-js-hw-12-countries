0c2fba055073091c2ec8c0e85d0fb807b3e40828
Alex34972 <alex_hifi@ukr.net>
2021-06-02 22:07:02 +0300

    Create get-refs.js

---
+++ src/js/get-refs.js
@@ -0,0 +1,9 @@
+export default function getRefs(){
+    return{
+        input: document.querySelector(".search"),
+        cardContainer:document.querySelector(".countries-card"),
+        sampleСantries:document.querySelector(".countries-search"),
+        form:document.querySelector(".alert")
+      };
+     
+}
