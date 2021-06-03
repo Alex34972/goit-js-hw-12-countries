4c1edfbf0fca9536a2f74169035a7955eba271fe
Alex34972 <alex_hifi@ukr.net>
2021-06-03 12:16:10 +0300

    Update alert.js

--- src/js/alert.js
+++ src/js/alert.js
@@ -1,4 +1,7 @@
 import { error } from "@pnotify/core";
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
+import "@pnotify/confirm/dist/PNotifyConfirm.css";
   function prompt(){
     error({
     text: "Too many matches found. Please enter a more specific query!",
