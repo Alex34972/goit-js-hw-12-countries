6e4a6116977ce965d91b7c09bd532df7524540fd
Alex34972 <alex_hifi@ukr.net>
2021-06-05 21:19:32 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,35 +1,34 @@
 import cantriesCardTempl from "../templates/cantries-card.hbs";
 import cantriesSearch from "../templates/search-cantries.hbs";
 
-import API from './api-service';
-import getRefs from './get-refs';
-import promt from './alert';
+import API from "./api-service";
+import getRefs from "./get-refs";
+import promt from "./alert";
 
 const refs = getRefs();
-let debounce = require('lodash.debounce');
-refs.input.addEventListener(`input`,debounce(onSearch,500));
+let debounce = require("lodash.debounce");
+refs.input.addEventListener(`input`, debounce(onSearch, 500));
 
 function onSearch(e) {
-    e.preventDefault();
-    const countries = e.target.value.trim();
-    console.log(countries)
-    if (isNaN(countries) === true) {
-      API.fetchCountries(countries)
-      .then(renderCard)
-    }
-    };
-  
-function renderCard(cantries){
+  e.preventDefault();
+  const countries = e.target.value.trim();
+  console.log(countries);
+  if (isNaN(countries) === true) {
+    API.fetchCountries(countries).then(renderCard);
+  }
+}
+
+function renderCard(cantries) {
   const marcup = cantriesCardTempl(...cantries);
- if (cantries.length===1) {
-  refs.cardContainer.innerHTML = marcup;
-  refs.sampleСantries.innerHTML = ``;
-  return;
- } else if (cantries.length>=2 && cantries.length<=10) {
-  const sample = cantriesSearch(cantries);
-  refs.sampleСantries.innerHTML= sample;
-  return;
- }else{
-   promt();
- 
-  }} 
+  if (cantries.length === 1) {
+    refs.cardContainer.innerHTML = marcup;
+    refs.sampleСantries.innerHTML = ``;
+    return;
+  } else if (cantries.length >= 2 && cantries.length <= 10) {
+    const sample = cantriesSearch(cantries);
+    refs.sampleСantries.innerHTML = sample;
+    return;
+  } else {
+    promt();
+  }
+}
